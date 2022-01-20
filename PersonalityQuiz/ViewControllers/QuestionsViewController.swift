55e5735a225c0ec3cddbfd933f3b2619d14cbe14
AlexeyB163 <sourceaisa@mail.ru>
2022-01-20 11:55:39 +0400

    logic for all vc - completed

--- PersonalityQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalityQuiz/ViewControllers/QuestionsViewController.swift
@@ -22,15 +22,131 @@ class QuestionsViewController: UIViewController {
     
     @IBOutlet weak var rangeStackView: UIStackView!
     @IBOutlet var rangeLabels: [UILabel]!
-    @IBOutlet weak var rangeSlider: UISlider!
+    @IBOutlet weak var rangeSlider: UISlider! {
+        didSet {
+            let answerCount = Float(currentAnswers.count - 1 )
+            rangeSlider.maximumValue = answerCount
+            rangeSlider.value = answerCount / 2
+            
+        }
+    }
     
-    
+    // MARK: - Private Property
     private let questions = Question.getQuestions()
+    
+    private var answersChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answer
+    }
+    private var questionIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
 
      }
     
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultVC = segue.destination as? ResultViewController else { return }
+        resultVC.answersUser = answersChosen
+        
+    }
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChosen.append(currentAnswer)
+        
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        for (multipleSwitch, answer) in zip(multipleSwitchs, currentAnswers ) {
+            if multipleSwitch.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func rangeAnswerButtonPressed() {
+        let index = lrintf(rangeSlider.value)
+        answersChosen.append(currentAnswers[index])
+        nextQuestion()
+    }
+    
+}
 
+// MARK: - Private Methods
+extension QuestionsViewController {
+    private func updateUI() {
+        
+        //Hide stack
+        for stackView in [singleStackView, multipleStackView, rangeStackView] {
+            stackView?.isHidden = true
+        }
+        
+        //Get current question
+        let currentQuestion = questions[questionIndex]
+        
+        //Set current question for question label
+        questionLabel.text = currentQuestion.title
+        
+        //Calculate progress
+        let totalProgress =  Float(questionIndex)/Float(questions.count)
+        
+        //Set progress for question progress view
+        questionsProgressView.setProgress(totalProgress, animated: true)
+        
+        //Set navigation title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single:
+            showSingleStackView(with:  currentAnswers)
+        case .multiple:
+            showMultipleStackView(with: currentAnswers)
+        case .range:
+            showRangeStackView(with: currentAnswers)
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal )
+        }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden.toggle()
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    private func showRangeStackView(with answers: [Answer]) {
+        rangeStackView.isHidden.toggle()
+        
+        rangeLabels.first?.text = answers.first?.title
+        rangeLabels.last?.text = answers.last?.title
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
+       
 }
--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -9,21 +9,82 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+  
+
+    @IBOutlet weak var userIconLabel: UILabel!
+    
+    @IBOutlet weak var discriptionLabel: UILabel!
+    
+    var answersUser:[Answer] = []
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.setHidesBackButton(true, animated: false)
+        setDiscriptionForAnswer(for: getFrequentAnswer(from: answersUser))
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ResultViewController {
+    private func getFrequentAnswer(from answers: [Answer]) -> [[Answer]] {
+        
+        var animals:[[Answer]] = []
+        
+        var cat:[Answer] = []
+        var dog:[Answer] = []
+        var rabbit:[Answer] = []
+        var turtle:[Answer] = []
+        
+        var maxCount: Int {
+            animals.map{$0.count}.max()!
+        }
+        var filterAnswer:[[Answer]]{
+            animals.filter{$0.count == maxCount}
+        }
+        
+        answers.forEach {
+            if $0.animal == .cat {
+                cat.append($0)
+                
+            } else if $0.animal == .dog {
+                dog.append($0)
+                
+            } else if $0.animal == .rabbit {
+                rabbit.append($0)
+                
+            } else {
+                turtle.append($0)
+                
+            }
+            
+            animals = [cat] + [dog] + [rabbit] + [turtle]
+        }
+        
+        return filterAnswer
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setDiscriptionForAnswer(for resultAnswers: [[Answer]]) {
+        
+        for i in resultAnswers {
+            var icon: String {
+                    "Вы - \(i[0].animal.rawValue)"
+                }
+            var definition: String {
+                    i[0].animal.definition
+                }
+            if i[0].animal == .dog {
+                userIconLabel.text = icon
+                discriptionLabel.text = definition
+            } else if i[0].animal == .cat {
+                userIconLabel.text = icon
+                discriptionLabel.text = definition
+            } else if i[0].animal == .rabbit {
+                userIconLabel.text = icon
+                discriptionLabel.text = definition
+            } else {
+                userIconLabel.text = icon
+                discriptionLabel.text = definition
+            }
+        }
     }
-    */
-
 }
